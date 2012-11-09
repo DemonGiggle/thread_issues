a20fd1b776d0fd12d2f2a0186f4e236bf5be0db8
giggle <giggle@zillians.com>
2012-11-09 16:33:20 +0800

    fix the last change

--- other.cpp
+++ other.cpp
@@ -2,12 +2,14 @@
 
 #include "other.h"
 #include <thread>
+#include <mutex>
 #include <time.h>
 #include <stdlib.h>
 
 std::thread* g_managed = NULL;
 bool g_finish = false;
 int expected_thread_count;
+std::mutex g_mtx;
 
 int get_expected_thread_count()
 {
@@ -18,24 +20,31 @@ void notify_each_run()
 {
     // sometimes, we just random to set the thread count to the expected value
     // to increase the possibility to somewhat racing condition
-    if (random() % 88 > 44)
+    if (g_mtx.try_lock())
     {
-        expected_thread_count = MAX_THREAD_COUNT * 2;
+        if (random() % 88 > 44)
+        {
+            expected_thread_count = MAX_THREAD_COUNT * 2;
+        }
+        else
+        {
+            expected_thread_count = MAX_THREAD_COUNT;
+        }
+        g_mtx.unlock();
     }
-    else
-    {
-        expected_thread_count = MAX_THREAD_COUNT;
-    }
-
 }
 
 void manage_func()
 {
     while (!g_finish)
     {
-        if (expected_thread_count != MAX_THREAD_COUNT)
+        if (g_mtx.try_lock())
         {
-            expected_thread_count--;
+            if (expected_thread_count != MAX_THREAD_COUNT)
+            {
+                expected_thread_count--;
+            }
+            g_mtx.unlock();
         }
         
         std::this_thread::yield();
